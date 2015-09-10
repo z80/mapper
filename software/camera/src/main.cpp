df97019ce160fefaeef44a8ef3c552d92fa5165a
unknown <z80@cortex-m3.(none)>
2015-09-09 17:01:43 -0700

    a

---
+++ software/camera/src/main.cpp
@@ -0,0 +1,77 @@
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void drawText(Mat & image);
+
+int main()
+{
+    cout << "Built with OpenCV " << CV_VERSION << endl;
+    Mat image;
+    VideoCapture capture;
+    capture.open(0);
+
+    FileStorage fs( "./data/out_camera_data.xml", FileStorage::READ); // Read the settings
+    if (!fs.isOpened())
+    {
+          cout << "Could not open the configuration file" << endl;
+          return -1;
+    }
+
+    Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+    Mat distCoeffs = Mat::zeros(5, 1, CV_64F);
+
+    fs[ "camera_matrix" ] >> cameraMatrix;
+    fs[ "distortion_coefficients" ] >> distCoeffs;
+    fs.release();        
+
+    if(capture.isOpened())
+    {
+        cout << "Capture is opened" << endl;
+        for(;;)
+        {
+            capture >> image;
+            if(image.empty())
+                break;
+            Mat undistorted = image.clone();
+            undistort( image, undistorted, cameraMatrix, distCoeffs );
+            //drawText(image);
+            imshow( "Original", image );
+            imshow( "Undistorted", undistorted );
+            int key = waitKey( 10 );
+            if( key == 'q' )
+                break;
+            else if (key == 's' )
+            {
+                static int ind = 0;
+                // Make screenshot.
+                ostringstream os;
+                os << "image" << ind++ << ".jpg";
+                imwrite( os.str(), image );
+            }
+        }
+    }
+    else
+    {
+        cout << "No capture" << endl;
+        image = Mat::zeros(480, 640, CV_8UC1);
+        drawText(image);
+        imshow("Sample", image);
+        waitKey(0);
+    }
+    return 0;
+}
+
+void drawText(Mat & image)
+{
+    putText(image, "Hello OpenCV",
+            Point(20, 50),
+            FONT_HERSHEY_COMPLEX, 1, // font face and scale
+            Scalar(255, 255, 255), // white
+            5, LINE_AA); // line thickness and type
+}
---
+++ software/settings.unix.cmake
@@ -0,0 +1,6 @@
+
+set( CROSS True CACHE TYPE BOOL )
+
+set( ICE_LIB_PATH "${ROOT}/.build/icee/cppe/lib" )
+set( ICEE_PATH "${ROOT}/.build/icee/cppe/include" )
+set( ICEE_UTIL_PATH "${ROOT}/.build/icee/cpp/include" )

93d6ddf6f1a2c8bd3234aed0d4bcaa067ae16d49
z80 <bashkirov.sergey@gmail.com>
2015-11-08 11:07:29 +0300

    a

--- analysis/tests.R
+++ analysis/tests.R
@@ -1,9 +1,10 @@
 setwd( "~/projects/mapper.git/software/.build.app" )
-workOrigData <- read.table( './data/points3d.txt' )
+workOrigData <- read.csv( './data/points3d.txt', header=F,sep=";" )
 names( workOrigData ) <- c( 'x', 'y', 'z', 'cx', 'cy', 'cz' )
 
 library( plot3D )
 library( plot3Drgl )
 
 d<-workOrigData
+d$z <- -as.numeric(d$z)
 plot3d( d$x, d$y, d$z )

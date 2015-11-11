895545d696d3b7fb31f20d8700a20fefec828ba2
z80 <bashkirov.sergey@gmail.com>
2015-11-11 10:45:07 +0300

    This #$@ doesn't work in principle. It works only in model situations. But with real images I constantly get #$@ :((((((

--- software/tests/test-flow/src/main.cpp
+++ software/tests/test-flow/src/main.cpp
@@ -15,13 +15,19 @@
 
 void drawText( cv::Mat & image, const std::string & stri, int line=0 );
 void initPoints( cv::Size sz );
-void appPoint( cv::Size sz, int row, int col, cv::Point2f displacement );
+void addPoint( cv::Size sz, int row, int col, cv::Point2f displacement );
 void framePoints();
+void mergeLastTracks();
+static void on_mouse( int event, int x, int y, int flags, void* param );
+void findAndDisplay( cv::Mat & img, int x, int y );
+void findPoint( std::vector<cv::Point2f> & pts );
+
+std::vector<cv::Mat> worldPts;
+cv::Mat cameraMatrix;
+cv::Mat distCoeffs;
 
 int main()
 {
-    cv::Mat cameraMatrix;
-    cv::Mat distCoeffs;
 
     {
         cv::FileStorage fs( "./data/out_camera_data.xml", cv::FileStorage::READ); // Read the settings
@@ -74,31 +80,44 @@ int main()
         sprintf( frameName, "pos%d", i );
         fs[ frameName ] >> cameraPos;
         //cout << (string)*it << "\n";
+        worldPts.push_back( cameraPos );
 
         cv::Mat image = cv::imread( fname );
 
         //if ( first )
         cv::Mat undistorted; // = image.clone();
         cv::undistort( image, undistorted, cameraMatrix, distCoeffs );
-
+        cv::flip( undistorted, undistorted, 1 );
 
 
         cvtColor( undistorted, gray, cv::COLOR_BGR2GRAY );
         if ( !grayPrev.empty() )
         {
-            cv::calcOpticalFlowFarneback( grayPrev, gray, uflow, 0.5, 8, 15, 3, 5, 1.2, 0 );
+            cv::calcOpticalFlowFarneback( grayPrev, gray, uflow, 0.5, 8, 25, 3, 5, 1.7, 0 );
             uflow.copyTo( flow );
-            cv::add( flowAccum, flow, flowAccum );
+            //cv::add( flowAccum, flow, flowAccum );
+            for(int y = 0; y < imgFirst.rows; y++)
+            {
+                for(int x = 0; x < imgFirst.cols; x++)
+                {
+                    cv::Point2f displacement = flow.at<cv::Point2f>(y, x);
+                    addPoint( cv::Size( imgFirst.cols, imgFirst.rows ),
+                              y, x, displacement );
+                }
+            }
         }
         grayPrev = gray.clone();
 
         if ( first )
         {
-            cv::calcOpticalFlowFarneback( gray, gray, uflow, 0.5, 3, 15, 3, 5, 1.2, 0 );
-            uflow.copyTo( flow );
-            flowAccum = flow.clone();
+            //cv::calcOpticalFlowFarneback( gray, gray, uflow, 0.5, 3, 15, 3, 5, 1.2, 0 );
+            //uflow.copyTo( flow );
+            //flowAccum = flow.clone();
             imgFirst = undistorted.clone();
             first = false;
+
+            // Initialize tracks.
+            initPoints( cv::Size( imgFirst.cols, imgFirst.rows ) );
         }
         else
         {
@@ -106,15 +125,22 @@ int main()
                     imgLast = undistorted.clone();
         }
 
+        framePoints();
+
         cv::imshow( "image", undistorted );
         cv::waitKey( 1 );
     }
 
     fs.release();
 
+
+    mergeLastTracks();
+
+/*
     int step = 64;
     cv::Scalar color = cv::Scalar(0, 255, 0);
     for(int y = 0; y < imgFirst.rows; y += step)
+    {
         for(int x = 0; x < imgFirst.cols; x += step)
         {
             const cv::Point2f & fxy = flowAccum.at<cv::Point2f>(y, x);
@@ -122,10 +148,11 @@ int main()
                  color);
             cv::circle(imgFirst, cv::Point(x,y), 2, color, -1);
         }
-
-
+    }
+*/
     cv::imshow( "first", imgFirst );
     cv::imshow( "last",  imgLast );
+    cv::setMouseCallback( "last", on_mouse, reinterpret_cast<void *>( &imgLast ) );
 
     cv::waitKey( 0 );
     return 0;
@@ -142,7 +169,7 @@ void drawText( cv::Mat & image, const std::string & stri, int line )
 
 
 std::map< int, std::vector<cv::Point2f> > frames;
-std::map< int, std::vector<cv::Point2f> > framesStopped;
+std::map< int, std::vector<cv::Point2f> > framesDone;
 std::map< int, std::vector<cv::Point2f> > framesNew;
 void initPoints( cv::Size sz )
 {
@@ -154,13 +181,13 @@ void initPoints( cv::Size sz )
             int index = row * sz.width + col;
             std::vector<cv::Point2f> pts;
             pts.push_back( pt );
-            frames[index] = pts;
+            framesNew[index] = pts;
         }
     }
 }
 
 
-void appPoint( cv::Size sz, int row, int col, cv::Point2f displacement )
+void addPoint( cv::Size sz, int row, int col, cv::Point2f displacement )
 {
     int index = row * sz.width + col;
     double x = static_cast<double>(col) + displacement.x;
@@ -170,17 +197,23 @@ void appPoint( cv::Size sz, int row, int col, cv::Point2f displacement )
     cv::Point2f pt = cv::Point2f( static_cast<float>(col) + displacement.x, static_cast<float>(row) + displacement.y );
     if ( it != frames.end() )
     {
-        if ( ( dabs( displacement.x ) >= 0.5 ) && 
-             ( dabs( displacement.y ) >= 0.5 ) )
+        std::vector<cv::Point2f> & pts = frames[index];
+        if ( ( fabs( displacement.x ) >= 0.5 ) &&
+             ( fabs( displacement.y ) >= 0.5 ) )
         {
-            std::vector<cv::Point2f> & pts = frames[index];
+            // If there is displacement place to further processing.
             pts.push_back( pt );
             framesNew[ newIndex ] = pts;
         }
         else
         {
-            int origIndex = .....;
-            framesDone[ origIndex ] = .....;
+            // Else put to completed chains.
+            if ( pts.size() > 0 )
+            {
+                cv::Point2f pt = pts[ 0 ];
+                int origIndex = sz.width * cvRound( pt.y ) + cvRound( pt.x );
+                framesDone[ origIndex ] = pts;
+            }
         }
     }
 }
@@ -190,7 +223,158 @@ void framePoints()
     frames = framesNew;
 }
 
+void mergeLastTracks()
+{
+    std::map< int, std::vector<cv::Point2f> >::const_iterator it;
+    for ( it=frames.begin(); it!=frames.end(); it++ )
+    {
+        framesDone[ it->first ] = it->second;
+    }
 
+    // Filter only meaningful tracks. E.i. with length > 1.
+    frames.clear();
+    for ( it=framesDone.begin(); it!=framesDone.end(); it++ )
+    {
+        if ( it->second.size() > 4 )
+            frames[ it->first ] = it->second;
+    }
+}
+
+static void on_mouse( int event, int x, int y, int flags, void* param )
+{
+    cv::Mat & img = *reinterpret_cast<cv::Mat *>( param );
+    switch( event )
+    {
+    case cv::EVENT_LBUTTONDOWN: // set rect or GC_BGD(GC_FGD) labels
+        findAndDisplay( img, x, y );
+        break;
+    default:
+        break;
+    }
+}
+
+void findAndDisplay( cv::Mat & img, int x, int y )
+{
+    cv::Mat painted = img.clone();
+    // Find closest point.
+    int index = -1;
+    double dist = 23456788.0;
+
+    int rows = painted.rows;
+    int cols = painted.cols;
+    std::map< int, std::vector<cv::Point2f> >::const_iterator it;
+    for ( it=frames.begin();
+          it!=frames.end(); it++ )
+    {
+        int i = it->first;
+        int row = i/cols;
+        int col = i - row*cols;
+        double dx = static_cast<double>( col - x );
+        double dy = static_cast<double>( row - y );
+        double d = sqrt( dx*dx + dy*dy );
+        if ( d < dist )
+        {
+            index = it->first;
+            dist = d;
+        }
+    }
+
+    // Draw track for this closest point.
+    std::vector<cv::Point2f> & pts = framesDone[ index ];
+    int sz = static_cast<int>( pts.size() );
+    for ( int i=1; i<sz; i++ )
+    {
+        cv::Point2f from = pts[i-1];
+        cv::Point2f to   = pts[i];
+        cv::line( painted, from, to, cv::Scalar( 0, 255, 0 ) );
+    }
+    cv::imshow( "processed", painted );
+    std::cout << "sz: " << sz << std::endl;
+
+    findPoint( pts );
+}
+
+void findPoint( std::vector<cv::Point2f> & pts )
+{
+    int sz = static_cast<int>( pts.size() );
+
+    cv::Mat A( 3*sz, 3, CV_64FC1 );
+    cv::Mat B( 3*sz, 1, CV_64FC1 );
+
+    double fx = cameraMatrix.at<double>( 0, 0 );
+    double fy = cameraMatrix.at<double>( 1, 1 );
+    double cx = cameraMatrix.at<double>( 0, 2 );
+    double cy = cameraMatrix.at<double>( 1, 2 );
+
+    for ( int i=0; i<sz; i++ )
+    {
+        cv::Point2f pt = pts[i];
+
+        cv::Mat m( 4, 1, CV_64FC1 );
+        double x = (pt.x - cx)/fx;
+        double y = (pt.y - cy)/fy;
+        double z = 1.0;
+        double l = sqrt( x*x + y*y + z*z );
+        x /= l;
+        y /= l;
+        z /= l;
+        m.at<double>( 0, 0 ) = x;
+        m.at<double>( 1, 0 ) = y;
+        m.at<double>( 2, 0 ) = z;
+        m.at<double>( 3, 0 ) = 0.0; // Yes, 0 to ignore translation part.
+
+        // Camera position.
+        double r0[3];
+        r0[0] = worldPts[i].at<double>( 0, 3 );
+        r0[1] = worldPts[i].at<double>( 1, 3 );
+        r0[2] = worldPts[i].at<double>( 2, 3 );
+
+        // Remember the very first camera position to find appropriate
+        // surface plane direction afterwards.
+        /*if ( i == 0 )
+        {
+            from.x = r0[0];
+            from.y = r0[1];
+            from.z = r0[2];
+        }*/
+
+        // Convert to world ref. frame.
+        //m = wrld*m;
+        double a[3];
+        a[0] = m.at<double>( 0, 0 );
+        a[1] = m.at<double>( 1, 0 );
+        a[2] = m.at<double>( 2, 0 );
+
+        A.at<double>( 3*i, 0 ) = 1.0 - a[0]*a[0];
+        A.at<double>( 3*i, 1 ) = -a[0]*a[1];
+        A.at<double>( 3*i, 2 ) = -a[0]*a[2];
+
+        A.at<double>( 3*i+1, 0 ) = -a[0]*a[1];
+        A.at<double>( 3*i+1, 1 ) = 1.0 - a[1]*a[1];
+        A.at<double>( 3*i+1, 2 ) = -a[1]*a[2];
+
+        A.at<double>( 3*i+2, 0 ) = -a[0]*a[2];
+        A.at<double>( 3*i+2, 1 ) = -a[1]*a[2];
+        A.at<double>( 3*i+2, 2 ) = 1.0 - a[2]*a[2];
+
+        B.at<double>( 3*i, 0 )   = (1.0 - a[0]*a[0])*r0[0] - a[0]*a[1]*r0[1] - a[0]*a[2]*r0[2];
+        B.at<double>( 3*i+1, 0 ) = -a[0]*a[1]*r0[0] + (1.0 - a[1]*a[1])*r0[1] - a[1]*a[2]*r0[2];
+        B.at<double>( 3*i+2, 0 ) = -a[0]*a[2]*r0[0] - a[1]*a[2]*r0[1] + (1.0 - a[2]*a[2])*r0[2];
+    }
+    cv::Mat Atr = A.t();
+    cv::Mat AtrA = Atr * A;
+    cv::Mat invAtrA = AtrA.inv();
+    //cv::Mat unity = invAtrA * AtrA;
+    cv::Mat AtrB = Atr * B;
+    cv::Mat R = invAtrA * AtrB;
+
+    cv::Point3d at;
+    at.x = R.at<double>( 0, 0 );
+    at.y = R.at<double>( 1, 0 );
+    at.z = R.at<double>( 2, 0 );
+
+    std::cout << "x: " << at.x << ", y: " << at.y << ", z: " << at.z << std::endl;
+}
 
 
 

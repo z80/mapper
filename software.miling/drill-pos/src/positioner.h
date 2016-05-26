47a86d342e8ea7c5cbc6a03c9ae77d43a9b4727b
unknown <z80@cortex-m3.(none)>
2016-05-25 17:02:00 -0700

    a

---
+++ software.miling/drill-pos/src/positioner.h
@@ -0,0 +1,40 @@
+
+#ifndef __POSITIONER_H_
+#define __POSITIONER_H_
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <vector>
+#include <iostream>
+
+class Positioner
+{
+public:
+    Positioner();
+    ~Positioner();
+
+    void frame( cv::Mat & img );
+    void resetPosition();
+
+    void startDrillPos();
+    void appendDrillPos( double & r[], double & n[] );
+    void endDrillPos();
+
+    void startLinePos();
+    void appendLinePos();
+    void endLinePos();
+
+private:
+    std::vector<cv::Point2d> knownPts;
+    double a[9]; // Ref. frame transformation.
+    double R[2]; // Drill position.
+    double d;    // Drill diameter.
+};
+
+
+#endif
+
+

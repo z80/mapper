10ec5a676d6b71469064f8c748018c88a13f4e9e
z80 <bashkirov.sergey@gmail.com>
2016-06-13 10:19:32 +0300

    Added CAM.

---
+++ software.miling/3dparty/camlib/src/cylcutter.cpp
@@ -0,0 +1,105 @@
+/*  $Id$
+ * 
+ *  Copyright 2010 Anders Wallin (anders.e.e.wallin "at" gmail.com)
+ *  
+ *  This file is part of OpenCAMlib.
+ *
+ *  OpenCAMlib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OpenCAMlib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenCAMlib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <boost/foreach.hpp>
+
+#include "cylcutter.hpp"
+#include "bullcutter.hpp" // for offsetCutter()
+#include "numeric.hpp"
+
+namespace ocl
+{
+
+CylCutter::CylCutter() {
+    std::cout << " usage: CylCutter( double diameter, double length ) \n";
+    assert(0);
+}
+
+CylCutter::CylCutter(double d, double l) {
+    diameter= d;                    assert( d > 0.0);
+    radius= d/2.0;
+    length = l;                     assert( l > 0.0 );
+    xy_normal_length = radius;
+    normal_length = 0.0;
+    center_height = 0.0;
+}
+
+// drop-cutter vertexDrop is handled by the base-class method in MillingCutter
+// drop-cutter facetDrop is handled by the base-class method in MillingCutter
+
+// we handle the edge-drop here.
+CC_CLZ_Pair CylCutter::singleEdgeDropCanonical(const Point& u1, const Point& u2) const {
+    // along the x-axis the cc-point is at x-coord s or -s:
+    double s = sqrt( square( radius ) - square( u1.y ) );
+    Point cc1(  s, u1.y, 0);
+    Point cc2( -s, u1.y, 0);
+    cc1.z_projectOntoEdge(u1,u2);
+    cc2.z_projectOntoEdge(u1,u2);
+    // pick the higher one
+    double cc_u;
+    double cl_z;
+    if (cc1.z > cc2.z) {
+        cc_u = cc1.x;
+        cl_z = cc1.z;
+    } else {
+        cc_u = cc2.x;
+        cl_z = cc2.z;
+    }
+    return CC_CLZ_Pair( cc_u, cl_z);
+}
+
+// general purpose vertexPush, delegates to this->width(h) 
+bool CylCutter::vertexPush(const Fiber& f, Interval& i, const Triangle& t) const {
+    bool result = false;
+    BOOST_FOREACH( const Point& p, t.p) {
+        if (this->singleVertexPush(f,i,p, VERTEX) )
+            result = true;
+    }
+
+    Point p1, p2;
+    if ( t.zslice_verts(p1, p2, f.p1.z) ) {
+        p1.z = f.p1.z; // z-coord should be very close to f.p1.z, but set it exactly anyway.
+        p2.z = f.p1.z;
+        if ( this->singleVertexPush(f,i,p1, VERTEX_CYL) )
+            result = true;
+        if (this->singleVertexPush(f,i,p2, VERTEX_CYL))
+            result = true;
+    }
+    
+    return result;
+}
+
+std::string CylCutter::str() const {
+    std::ostringstream o;
+    o << *this;
+    return o.str();
+}
+
+std::ostream& operator<<(std::ostream &stream, CylCutter c) {
+  stream << "CylCutter (d=" << c.diameter << ", L=" << c.length <<  ")";
+  return stream;
+}
+
+} // end namespace
+// end file cylcutter.cpp
---
+++ software.miling/3dparty/camlib/src/weave.cpp
@@ -0,0 +1,121 @@
+/*  
+ *  Copyright 2010-2011 Anders Wallin (anders.e.e.wallin "at" gmail.com)
+ *  
+ *  This file is part of OpenCAMlib.
+ *
+ *  OpenCAMlib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OpenCAMlib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenCAMlib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "weave.hpp"
+
+
+namespace ocl
+{
+
+namespace weave
+{
+
+int VertexProps::count = 0;
+
+void Weave::addFiber(Fiber& f) {
+    if ( f.dir.xParallel() && !f.empty() ) {
+        xfibers.push_back(f);
+    } else if ( f.dir.yParallel() && !f.empty() ) {
+        yfibers.push_back(f);
+    } else if (!f.empty()) {
+        assert(0); // fiber must be either x or y
+    }
+}
+
+// traverse the graph putting loops of vertices into the loops variable
+// this figure illustrates next-pointers: http://www.anderswallin.net/wp-content/uploads/2011/05/weave2_zoom.png
+void Weave::face_traverse() { 
+    std::cout << " traversing graph with " << clVertexSet.size() << " cl-points\n";
+    while ( !clVertexSet.empty() ) { // while unprocessed cl-vertices remain
+        std::vector<Vertex> loop; // start on a new loop
+        Vertex current = *(clVertexSet.begin());
+        Vertex first = current;
+        
+        do { // traverse around the loop
+            assert( g[current].type == CL ); // we only want cl-points in the loop
+            loop.push_back(current);
+            clVertexSet.erase(current); // remove from set of unprocesser cl-verts
+            std::vector<Edge> outEdges = g.out_edges(current);  // find the edge to follow
+            //if (outEdges.size() != 1 )
+            //    std::cout << " outEdges.size() = " << outEdges.size() << "\n";
+            assert( outEdges.size() == 1 ); // cl-points are allways at ends of intervals, so they have only one out-edge
+            Edge currentEdge = outEdges[0]; 
+            do { // following next, find a CL point 
+                current = g.target(currentEdge); 
+                currentEdge = g[currentEdge].next;
+            } while ( g[current].type != CL );
+        } while (current!=first); // end the loop when we arrive at the start
+        
+        loops.push_back(loop); // add the processed loop to the master list of all loops
+    }
+}
+
+
+std::vector< std::vector<Point> > Weave::getLoops() const {
+    std::vector< std::vector<Point> > loop_list;
+    BOOST_FOREACH( std::vector<Vertex> loop, loops ) {
+        std::vector<Point> point_list;
+        BOOST_FOREACH( Vertex v, loop ) {
+            point_list.push_back( g[v].position );
+        }
+        loop_list.push_back(point_list);
+    }
+    return loop_list;
+}
+
+
+// this can cause a build error when both face and vertex descriptors have the same type
+// i.e. unsigned int (?)
+// operator[] below "g[*itr].type" then looks for FaceProps.type which does not exist...
+void Weave::printGraph()  {
+    std::cout << " number of vertices: " <<  g.num_vertices() << "\n"; 
+    std::cout << " number of edges: " << g.num_edges()  << "\n"; 
+
+    int n=0, n_cl=0, n_internal=0;
+    BOOST_FOREACH(Vertex v, g.vertices() ) {
+        if ( g[v].type == CL )
+            ++n_cl;
+        else
+            ++n_internal;
+        ++n;
+    }
+    
+    std::cout << " counted " << n << " vertices\n";
+    std::cout << "          CL-nodes: " << n_cl << "\n";
+    std::cout << "    internal-nodes: " << n_internal << "\n";
+}
+        
+// string representation
+std::string Weave::str()  {
+    std::ostringstream o;
+    o << "Weave2\n";
+    o << "  " << xfibers.size() << " X-fibers\n";
+    o << "  " << yfibers.size() << " Y-fibers\n";
+    return o.str();
+}
+        
+
+} // end weave namespace
+
+} // end ocl namespace
+// end file weave.cpp

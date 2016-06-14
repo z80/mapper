6c5b13a28b0472bc8e9a822c99d5abf76782a443
unknown <z80@cortex-m3.(none)>
2016-06-14 13:24:29 -0700

    a

---
+++ software.miling/drill_gui/src/model.cpp
@@ -0,0 +1,69 @@
+
+#include "model.h"
+
+Model::Model()
+{
+    ptsM      = vtkPoints::New();
+    polyDataM = vtkPolyData::New();
+    polyDataM->Allocate();
+    mapperM = vtkPolyDataMapper::New();
+    mapperM->SetInputData( polyDataM );
+    actorM = vtkActor::New();
+    actorM->SetMapper( mapperM );
+    actorM->GetProperty()->SetColor( 0.0, 0.75, 0.75 );
+
+    ptsS      = vtkPoints::New();
+    polyDataS = vtkPolyData::New();
+    polyDataS->Allocate();
+    mapperS = vtkPolyDataMapper::New();
+    mapperS->SetInputData( polyDataS );
+    actorS = vtkActor::New();
+    actorS->SetMapper( mapperS );
+    actorS->GetProperty()->SetColor( 0.0, 0.75, 0.0 );
+}
+
+Model::~Model()
+{
+}
+
+void Model::loadModel( const std::string & fname )
+{
+    std::wstring wfname( fname.begin(), fname.end() );
+    ocl::STLReader reader( wfname, modelOrig );
+}
+
+void Model::loadSample( const std::string & fname )
+{
+    std::wstring wfname( fname.begin(), fname.end() );
+    ocl::STLReader reader( wfname, sampleOrig );
+}
+
+void Model::setModeSampleFace()
+{
+}
+
+void Model::setModeSampleEdge()
+{
+}
+
+void Model::setModeModelFace()
+{
+}
+
+void Model::setModeModelEdge()
+{
+}
+
+void Model::dropOnFace()
+{
+}
+
+void Model::alignToEdge()
+{
+}
+
+
+
+
+
+
---
+++ software.miling/drill_gui/src/model.h
@@ -0,0 +1,91 @@
+
+#ifndef __MODEL_H_
+#define __MODEL_H_
+
+#include "vtkConeSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkCamera.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
+#include "vtkProperty.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkInteractorStyleMultiTouchCamera.h"
+
+#include <vtkVersion.h>
+#include <vtkSmartPointer.h>
+#include <vtkRendererCollection.h>
+#include <vtkDataSetMapper.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkIdTypeArray.h>
+#include <vtkTriangleFilter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkCommand.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyData.h>
+#include <vtkPoints.h>
+#include <vtkCellArray.h>
+#include <vtkPlaneSource.h>
+#include <vtkCellPicker.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkProperty.h>
+#include <vtkSelectionNode.h>
+#include <vtkSelection.h>
+#include <vtkExtractSelection.h>
+#include <vtkObjectFactory.h>
+
+#include <stlsurf.hpp>
+#include <stlreader.hpp>
+#include <triangle.hpp>
+
+#include <string>
+#include <climits>
+
+
+
+class Model
+{
+public:
+    Model();
+    ~Model();
+
+    void loadModel( const std::string & fname );
+    void loadSample( const std::string & fname );
+
+    void setModeSampleFace();
+    void setModeSampleEdge();
+    void setModeModelFace();
+    void setModeModelEdge();
+
+    void dropOnFace();
+    void alignToEdge();
+
+    ocl::STLSurf modelOrig,  modelFlipped,  model;
+    ocl::STLSurf sampleOrig, sampleFlipped, sample;
+    // Flipping matrix.
+    double A[3][4];
+
+    // Orienting and positioning matrix.
+    double B[3][4];
+
+    // Visualization data.
+    vtkSmartPointer<vtkPoints>         ptsM;
+    vtkSmartPointer<vtkPolyData>       polyDataM;
+    vtkSmartPointer<vtkPolyDataMapper> mapperM;
+    vtkSmartPointer<vtkActor>          actorM;
+
+    vtkSmartPointer<vtkPoints>         ptsS;
+    vtkSmartPointer<vtkPolyData>       polyDataS;
+    vtkSmartPointer<vtkPolyDataMapper> mapperS;
+    vtkSmartPointer<vtkActor>          actorS;
+};
+
+
+
+
+#endif
+
+

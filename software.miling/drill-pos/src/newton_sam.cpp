e8a937fe335f912f8b05fd2a25f3fc82e3665d82
unknown <z80@cortex-m3.(none)>
2016-06-28 15:21:19 -0700

    a

--- software.miling/drill-pos/src/newton_sam.cpp
+++ software.miling/drill-pos/src/newton_sam.cpp
@@ -1,6 +1,8 @@
 
 
 #include "newton_sam.h"
+#include <algorithm>
+#include <numeric>
 
 const double NewtonSam::ALPHA        = 0.2;
 const double NewtonSam::MIN_STEP     = 1.0e-6;
@@ -18,7 +20,35 @@ NewtonSam::~NewtonSam()
 
 bool NewtonSam::matchPoints( std::vector<double> & pts, double d, cv::Mat & floor2Sample )
 {
+    // Points number.
     auto sz = pts.size() / 6;
+    // Sort points by lines.
+    std::vector< std::vector<double> > p;
+    for ( auto i=0; i<sz; )
+    {
+        std::vector<double> v( 6 );
+        for ( auto j=0; j<6; j++ )
+        {
+            v[j] = pts[i];
+            i++;
+        }
+        p.push_back( v );
+    }
+    std::vector< std::vector< std::vector<double> > > lines;
+    while ( p.size() > 0 )
+    {
+        double rx = p[0][2];
+        double ry = p[0][3];
+        double nx = p[0][4];
+        double ny = p[0][5];
+        std::vector< std::vector<double> > to;
+        std::remove_copy_if( p.begin(), p.end(), to.begin(), [=](const std::vector<double> & v) { return ( (v[2]==rx) && (v[3]==ry) && (v[4]==nx) && (v[5]==ny) ); } );
+        lines.push_back( to );
+    }
+    // For each line derive closest line.
+
+
+
     this->pts.clear();
     cv::Mat   X( sz, 6, CV_64F );
     cv::Mat   Y( sz, 1, CV_64F );

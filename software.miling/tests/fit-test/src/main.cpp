ecf7059aa341cf2a50d4c6af956e13deb19d059c
unknown <z80@cortex-m3.(none)>
2016-06-29 16:59:45 -0700

    a

--- software.miling/tests/fit-test/src/main.cpp
+++ software.miling/tests/fit-test/src/main.cpp
@@ -13,6 +13,38 @@
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/videoio/videoio.hpp"
 
+class Line
+{
+public:
+    Line() {}
+    ~Line() {}
+    Line( const Line & line )
+    {
+        *this = line;
+    }
+
+    const Line & operator=( const Line & line )
+    {
+        if ( this != &line )
+        {
+            ni   = line.ni;
+            ri   = line.ri;
+
+            pts = line.pts;
+            r   = line.r;
+            a   = line.a;
+        }
+        return *this;
+    }
+
+    void fitLine();
+
+    cv::Point2d ni, ri; // Declared line parameters; "i" from "ideal" word.
+
+    std::vector< cv::Point2d > pts; // Obtained points close to line.
+    cv::Point2d r, a; // Derived line parameteres.
+};
+
 void fitLine( std::vector< std::vector<double> > & line, cv::Point2d & c, cv::Point2d & a );
 
 int main()
@@ -67,18 +99,48 @@ int main()
     std::sort( lines.begin(), lines.end(), [=]( std::vector< std::vector<double> > & a, std::vector< std::vector<double> > & b ) { return (a.size () < b.size()); } );
     // The very first line hast the most points.
     // Approximate it with line using eigenvalues.
+    sz = lines.size();
+    std::vector<Line> linesS;
+    for ( auto i=0; i<sz; i++ )
+    {
+        Line line;
+        std::vector< std::vector<double> > & l = lines[i];
+        auto lsz = l.size();
+        for ( auto j=0; j<lsz; j++ )
+        {
+            std::vector<double> & pt = l[j];
+            line.ri = cv::Point2d( pt[2], pt[3] );
+            line.ni = cv::Point2d( pt[4], pt[5] );
+            line.pts.push_back( cv::Point2d( pt[0], pt[1] ) );
+        }
+        line.fitLine();
+        linesS.push_back( line );
+    }
 
 
    return 0;
 }
 
-void fitLine( std::vector< std::vector<double> > & line, cv::Point2d & c, cv::Point2d & a )
+void Line::fitLine()
 {
-    auto sz = line.size();
+    auto sz = pts.size();
+    if ( sz < 2 )
+        return;
+
     cv::Mat x( sz, 2, CV_64F );
     for ( auto i=0; i<sz; i++ )
     {
-        x.at<> = line[i][0];
-        x.at<> = line[i][1];
+        x.at<double>(i, 0) = pts[i].x;
+        x.at<double>(i, 1) = pts[i].y;
     }
+    
+    cv::PCA pca_analysis( x, cv::Mat(), CV_PCA_DATA_AS_ROW );
+
+    r = cv::Point2d( pca_analysis.mean.at<double>(0, 0), pca_analysis.mean.at<double>(0, 1) );
+
+    double e0 = pca_analysis.eigenvalues.at<double>(0, 0);
+    double e1 = pca_analysis.eigenvalues.at<double>(0, 1);
+
+    int ind = ( fabs( e0 ) > fabs( e1 ) ) ? 0 : 1;
+    a = cv::Point2d( pca_analysis.eigenvectors.at<double>(ind, 0), pca_analysis.eigenvectors.at<double>(ind, 1) );
 }

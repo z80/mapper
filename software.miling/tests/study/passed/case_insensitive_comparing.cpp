f4467a021cdd9ca285d75dc9e405222579db6158
z80 <bashkirov.sergey@gmail.com>
2016-07-20 10:29:00 +0300

    a

---
+++ software.miling/tests/study/passed/case_insensitive_comparing.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <string>
+
+template<typename T> struct Tr: public std::char_traits<T>
+{
+public:
+    static int compare(const T* s1, const T* s2, size_t n)
+    {
+        for ( auto i=0; i<n; i++ )
+        {
+            T ss1 = ( ( s1[i] >= 'A' ) && (s1[i] <= 'Z') ) ? (s1[i] + 'a' - 'A') : s1[i];
+            T ss2 = ( ( s2[i] >= 'A' ) && (s2[i] <= 'Z') ) ? (s2[i] + 'a' - 'A') : s2[i];
+
+            if ( ss1 < ss2 )
+                return -1;
+            else if ( ss1 > ss2 )
+                return 1;
+        }
+        return 0;
+    }
+};
+
+//using String = std::basic_string<char, Tr<char> >;
+
+int main(){
+    // std::cout << (String( "a" ) > String( "A" ) ) << std::endl;
+
+    return 0;
+}

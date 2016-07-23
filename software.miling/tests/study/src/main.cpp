7ea8999d668cddf434473f8a1dafb0383934512d
unknown <z80@cortex-m3.(none)>
2016-07-22 17:55:03 -0700

    a

--- software.miling/tests/study/src/main.cpp
+++ software.miling/tests/study/src/main.cpp
@@ -6,18 +6,78 @@
 #include <iostream>
 #include <string>
 
-
-void printingAllPermutations( const std::string & stri )
+class C
 {
-    std::string s = stri;
-    std::sort( s.begin(), s.end() );
-    do {
-        std::cout << s << std::endl;
-    } while ( std::next_permutation( s.begin(), s.end() ) );
-}
+public:
+    C()
+    {
+        protectedInstance = 0;
+        en = true;
+    }
+    C( const C & inst )
+    {
+        *this = inst;
+    }
+    virtual ~C()
+    {
+    }
+    const C & operator=( const C & inst )
+    {
+    }
+    static void setPretected( bool en )
+    {
+        this->en = en;
+        if ( !en )
+        {
+            if ( procectedInstance )
+            {
+                delete pretectedInstance;
+                protectedInstance = 0;
+            }
+        }
+    }
+
+
+
+public:
+    void * operator new( size_t qty )
+    {
+        void * ptr = malloc( qty );
+        if ( !ptr )
+            throw "Err mem allocation";
+        if ( en )
+            protectedInstance = ptr;
+    }
+    void  operator delete( void * obj )
+    {
+        free( obj );
+        if ( en )
+            protectedInstance = 0;
+    }
+private:
+    static C * protectedInstance;
+    static bool en;
+};
+
 
 
 int main(){
-    printingAllPermutations( "Hello!" );
+    C::setProtected( true );
+    C * c = new C();
+    //std::shared_ptr<C> sc( c );
+    //c = 0;
+    // Even more it is necessary to use mutexes 
+    // to prevent user of the code to try do something 
+    // with the pointer in a separate thread.
+    try {
+        // Working code here.
+    } except( ... )
+    {
+    }
+
+    C::setProtected( false );
     return 0;
 }
+
+
+

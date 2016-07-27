b6cd81fbfb7b029ef5491fe4bc9f1dd0213f1912
z80 <bashkirov.sergey@gmail.com>
2016-07-27 10:46:53 +0300

    Tested overwriting global new() and delete() operators.

--- software.miling/tests/study/src/main.cpp
+++ software.miling/tests/study/src/main.cpp
@@ -18,74 +18,34 @@
 #include <iostream>
 //using namespace boost::filesystem;
 
-const int ABS_MAX = 10;
-const int DIFF  = 1;
-// Minimum array adjustment.
-
-bool valid( int * A, int n )
+class A
 {
-    bool correct = true;
-    for ( auto i=1; i<n; i++ )
-    {
-        if ( abs(A[i] - A[i-1]) != DIFF )
-            return false;
-    }
-    return true;
+public:
+    A() {}
+    ~A() {}
+
+    int i;
+    int j;
+};
+
+void * operator new( size_t sz )
+{
+    void * p = malloc( sz );
+    std::cout << "new" << std::endl;
+    return p;
 }
 
-bool overflow( int * A, int n )
+void operator delete( void * p )
 {
-    for ( auto i=0; i<n; i++ )
-    {
-        if ( A[i] > ABS_MAX )
-            return false;
-    }
-}
-
-int spent( int * A, int n )
-{
-    if ( n <= 1 )
-        return 0;
-    if ( n == 2 )
-    {
-        if ( A[0] < A[1] )
-        {
-            int sum = A[1] - A[0] - 1;
-            A[0] = A[0] + sum;
-            return sum;
-        }
-        else
-        {
-            int sum = A[0] - A[1] - 1;
-            A[1] = A[1] + sum;
-            return sum;
-        }
-    }
-
-    int szRest = spent( A+1, n-1 );
-    if ( A[0] < A[1] )
-    {
-        int sum = A[1] - A[0] - 1;
-        A[0] = A[0] + sum;
-        return sum+szRest;
-    }
-    else
-    {
-        int sum = A[0] - A[1] - 1;
-        for ( auto i=1; i<n; i++ )
-            A[i] = A[i] + sum;
-        return sum*(n-1) + szRest;
-    }
-
-    return 0;
+    std::cout << "delete" << std::endl;
+    free( p );
 }
 
 
 int main()
 {
-    int A[] = { 3, 2, 6, 0, 1 };
-    int sz = spent( A, sizeof(A)/sizeof(int) );
-    std::cout << sz << std::endl;
+    A * a = new A();
+    delete a;
     return 0;
 }
 

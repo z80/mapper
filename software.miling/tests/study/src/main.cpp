fd5585061680d7de1f132457956d65e88d245b75
unknown <z80@cortex-m3.(none)>
2016-08-23 16:23:45 -0700

    a

--- software.miling/tests/study/src/main.cpp
+++ software.miling/tests/study/src/main.cpp
@@ -20,131 +20,115 @@
 #include <iostream>
 #include <queue>
 #include <map>
+#include <hash_map>
 //using namespace boost::filesystem;
 
-// Longest path in matrix avoiding obstacles.
-
-int pathFromPoint( const std::vector< std::vector<int> > & A, std::vector< std::vector<bool> > & visited, std::list< std::pair<int,int> > & path, int y, int x )
+class Baloons
 {
-    if ( ( visited[y][x] ) || ( A[y][x] != 0 ) )
-        return 0;
-
-    visited[y][x] = true;
-
-    // And check neighbour points.
-    int maxLen = 0;
-    std::list< std::pair<int,int> > p, maxPath;
-    if ( x > 0 )
+public:
+    Baloons( const std::list<int> & vals )
     {
-        int len = pathFromPoint( A, visited, p, y, x-1 );
-        maxLen = len;
-        maxPath = p;
+        baloons = vals;
     }
-    if ( x < A[0].size()-1 )
+
+    int theBestOrder( std::list<int> & order )
     {
-        p.clear();
-        int len = pathFromPoint( A, visited, p, y, x+1 );
-        if ( maxLen < len )
+        //std::cout << "entered: "; 
+        //for ( auto i=baloons.begin(); i!=baloons.end(); i++ )
+        //    std::cout << *i << ", ";
+        //std::cout << std::endl;
+
+        int depth = order.size();
+        int bestInd = -1;
+        int bestVal = -1;
+        int sz = baloons.size();
+        if ( sz < 2 )
         {
-            maxLen = len;
-            maxPath = p;
+            int val = baloons.back();
+            order.push_back( val );
+            return val;
         }
-    }
-    if ( y > 0 )
-    {
-        p.clear();
-        int len = pathFromPoint( A, visited, p, y-1, x );
-        if ( maxLen < len )
+        for ( auto i=0; i!=sz; i++ )
         {
-            maxLen = len;
-            maxPath = p;
-        }
-    }
-    if ( y < A.size()-1 )
-    {
-        p.clear();
-        int len = pathFromPoint( A, visited, p, y+1, x );
-        if ( maxLen < len )
-        {
-            maxLen = len;
-            maxPath = p;
-        }
-    }
+            int j=0;
+            auto iter = baloons.begin();
+            while ( j++ < i )
+                iter++;
+            int origVal = *iter;
+            int val  = origVal;
 
-    // Clear visitness of current point.
-    visited[y][x] = false;
-
-    // Append path with current point and found path from current point.
-    int len = 1 + maxPath.size();
-    path.push_back( std::pair<int, int>( y, x ) );
-    while ( maxPath.size() > 0 )
-    {
-        path.push_back( maxPath.front() );
-        maxPath.pop_front();
-    }
-
-    return len;
-}
-
-int maxPath( const std::vector< std::vector<int> > & A, std::list< std::pair<int,int> > & path )
-{
-    std::vector< std::vector<bool> > visited;
-    std::for_each( A.begin(), A.end(), [&]( const std::vector<int> & a )
-    {
-        std::vector<bool> v;
-        v.resize( a.size(), false );
-        visited.push_back( v );
-    } );
-
-    int rows = A.size();
-    int cols = A[0].size();
-    int maxLen = 0;
-    std::list< std::pair<int,int> > p;
-    for ( int y=0; y<rows; y++ )
-    {
-        for ( int x=0; x<cols; x++ )
-        {
-            p.clear();
-            int len = pathFromPoint( A, visited, p, y, x );
-            if ( len > maxLen )
+            auto prev = iter;
+            if ( iter != baloons.begin() )
             {
-                maxLen = len;
-                path = p;
+                prev--;
+                val *= *prev;
             }
+
+            auto next = iter;
+            if ( iter != baloons.end() )
+            {
+                next++;
+                if ( next != baloons.end() )
+                    val *= *next;
+            }
+
+            baloons.erase( iter );
+
+            std::list<int> bestList;
+            val += theBestOrder( bestList );
+            if ( val > bestVal )
+            {
+                bestInd = i;
+                bestVal = val;
+                while ( order.size() > depth )
+                    order.pop_back();
+                order.push_back( origVal );
+                while ( bestList.size() )
+                {
+                    order.push_back( bestList.front() );
+                    bestList.pop_front();
+                }
+            }
+            // Return value in it's place.
+            
+            iter = baloons.begin();
+            j=0;
+            while ( j++ < i )
+                iter++;
+            baloons.insert( iter, origVal );
         }
+
+        //for ( auto i=baloons.begin(); i!=baloons.end(); i++ )
+        //    std::cout << *i << ", ";
+        //std::cout << "val: " << bestVal << std::endl;
+
+        return bestVal;
     }
 
-    return maxLen;
-}
+    std::list<int> baloons;
+};
+
 
 
 
 int main()
 {
-    std::vector< std::vector<int> > A;
-    std::vector<int> a;
-    a.push_back( 0 ); a.push_back( 0 ); a.push_back( 1 ); a.push_back( 1 );
-    A.push_back( a );
-    a.clear();
-    a.push_back( 0 ); a.push_back( 1 ); a.push_back( 0 ); a.push_back( 0 );
-    A.push_back( a );
-    a.clear();
-    a.push_back( 0 ); a.push_back( 0 ); a.push_back( 0 ); a.push_back( 1 );
-    A.push_back( a );
-    a.clear();
-    a.push_back( 1 ); a.push_back( 0 ); a.push_back( 0 ); a.push_back( 1 );
-    A.push_back( a );
-    a.clear();
-
-    std::list< std::pair<int,int> > path;
-    int sz = maxPath( A, path );
-    std::cout << "path size: " << sz << std::endl;
-    std::for_each( path.begin(), path.end(), [&]( std::pair<int, int> & arg ) 
+    std::list<int> l;
+    l.push_back( 1 );
+    l.push_back( 5 );
+    l.push_back( 8 );
+    l.push_back( 11 );
+    l.push_back( 1 );
+    l.push_back( 2 );
+    l.push_back( 12 );
+    Baloons b( l );
+    l.clear();
+    int val = b.theBestOrder( l );
+    for ( auto i=l.begin(); i!=l.end(); i++ )
     {
-        std::cout << "( " << arg.first << ", " << arg.second << " ), ";
-    } );
-    std::cout << std::endl;
-
+        std::cout << *i << ", ";
+    }
+    std::cout << "result: " << val << std::endl;
     return 0;
 }
 

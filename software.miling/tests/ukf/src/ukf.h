a0dccae177112d8b6b4162c46ba9c09430d230c2
unknown <z80@cortex-m3.(none)>
2016-10-14 11:31:42 -0700

    a

--- software.miling/tests/ukf/src/ukf.h
+++ software.miling/tests/ukf/src/ukf.h
@@ -23,13 +23,33 @@ public:
 
     // Sigma point parameters.
     Float alpha; // Scaling parameters. 1<=alpha<=1+1e-4.
+    Float beta;  // For Gaussian distribution beta = 2 is optimal.
     Float k;     // k is usually set to either 0 or 3-L.
     // Lambda = alpha^2 * (L + k) - L.
 
+    // Process description "Rx", "Rz" process noise and sensor noise covariance matrices.
+    Float Rx[Nst][Nst];
+    Float Rz[Nsen][Nsen];
+
     Float x[Nst];     // State at previous step.
-    Float P[Nst*Nst]; // State noise covariance.
-    Float L[Nst*Nst]; // Square root of covariance, e.i. P = L * tr(L);
+    Float P[Nst][Nst]; // State noise covariance.
+    Float L[Nst][Nst]; // Square root of covariance, e.i. P = L * tr(L);
     Float sigma[2*Nst + 1][Nst]; // Sigma points.
+    Float Wm0; // Sigma point weights.
+    Float Wc0;
+    Float Wmci;
+    Float y[2*Nst + 1][Nst]; // Sigma points moved through predition function.
+    Float Ym[Nst];           // Mean value.
+    Float Py[Nst][Nst];       // Covariance matrix calculated from sigma points.
+
+    // Sensor readings.
+    Float z[2*Nst+1][Nsen];
+    Float Zm[Nsen];
+    Float Pz[Nsen][Nsen];
+    Float invPz[Nsen][Nsen];
+
+    // Combined covariance.
+    Float Pyz[Nst][Nsen];
 };
 
 

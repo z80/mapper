ec2bee87cc2af34f395a6676849b429e98d7bb46
z80 <bashkirov.sergey@gmail.com>
2016-10-17 23:29:20 -0700

    Compiles but calculates state covariance in wrong way. It becomes negative. Probably it is caused by wrong W values.

--- software.miling/tests/ukf/src/main.cpp
+++ software.miling/tests/ukf/src/main.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <iostream>
 #include <limits>
+#include <functional>
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
@@ -17,13 +18,44 @@
 
 #include "ukf.h"
 
+void pr( double * x, double * xn );
+void sen( double * x, double * z );
+
 int main()
 {
+    Ukf<double, 1, 1 > ukf;
+    ukf.init();
+    ukf.Rx[0][0] = 2.0; // cm;
+    ukf.Rz[0][0] = 2.0; // cm;
 
 
+    double x[1];
+    x[0] = 0.0;
+    double z[1];
+    z[0] = 0.0;
+    srand( 0 );
+
+    for ( auto i=0; i<100; i++ )
+    {
+        x[0] = static_cast<double>( ((rand() % 128) - 64 ) ) / 64.0;
+        z[0] = static_cast<double>( ((rand() % 128) - 64 ) ) / 64.0;
+        ukf.predict( x, std::bind( &pr, std::placeholders::_1, std::placeholders::_2 ) );
+        ukf.correct( z, sen );
+    }
 
     return 0;
 }
+
+void pr( double * x, double * xn )
+{
+    xn[0] = x[0];
+}
+
+void sen( double * x, double * z )
+{
+    z[0] = x[0];
+}
+
 
 
 
--- software.miling/tests/ukf/src/ukf.h
+++ software.miling/tests/ukf/src/ukf.h
@@ -6,6 +6,9 @@
 #include <algorithm>
 #include <numeric>
 #include <functional>
+#include "matrix2.hpp"
+
+
 
 template <typename Float, int Nst, int Nsen >
 class Ukf
@@ -17,9 +20,10 @@ public:
     Ukf();
     ~Ukf();
 
-    void predict( Float * x, FPredict & pr );
-    void correct( Float * z, FSens    & sens );
-    void cholesky( Float * P, Float * L );
+    void init();
+    void predict( Float * x, FPredict pr );
+    void correct( Float * z, FSens    sens );
+    void cholesky();
 
     // Sigma point parameters.
     Float alpha; // Scaling parameters. 1<=alpha<=1+1e-4.
@@ -35,15 +39,12 @@ public:
     Float P[Nst][Nst]; // State noise covariance.
     Float L[Nst][Nst]; // Square root of covariance, e.i. P = L * tr(L);
     Float sigma[2*Nst + 1][Nst]; // Sigma points.
-    Float Wm0; // Sigma point weights.
-    Float Wc0;
-    Float Wmci;
     Float y[2*Nst + 1][Nst]; // Sigma points moved through predition function.
     Float Ym[Nst];           // Mean value.
-    Float Py[Nst][Nst];       // Covariance matrix calculated from sigma points.
+    Float Py[Nst][Nst];      // Covariance matrix calculated from sigma points.
 
     // Sensor readings.
-    Float z[2*Nst+1][Nsen];
+    Float zy[2*Nst+1][Nsen];
     Float Zm[Nsen];
     Float Pz[Nsen][Nsen];
     Float invPz[Nsen][Nsen];
@@ -51,6 +52,271 @@ public:
     // Combined covariance.
     Float Pyz[Nst][Nsen];
 };
+
+
+
+
+
+template <typename Float, int Nst, int Nsen>
+Ukf<Float, Nst, Nsen>::Ukf()
+{
+}
+
+template <typename Float, int Nst, int Nsen>
+Ukf<Float, Nst, Nsen>::~Ukf()
+{
+}
+
+template <typename Float, int Nst, int Nsen>
+void Ukf<Float, Nst, Nsen>::init()
+{
+    alpha = 1.0; // Scaling parameters. 1<=alpha<=1+1e-4.
+    beta = 2.0;  // For Gaussian distribution beta = 2 is optimal.
+    k = 0.0;     // k is usually set to either 0 or 3-L.
+
+
+    for ( auto i=0; i<Nst; i++ )
+        x[i] = 0.0;
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nst; j++ )
+        {
+            P[i][j] = (i==j) ? 1.0 : 0.0;
+            Rx[i][j] = (i==j) ? 1.0 : 0.0;
+        }
+    }
+    for ( auto i=0; i<Nsen; i++ )
+    {
+        for ( auto j=0; j<Nsen; j++ )
+        {
+            Rz[i][j] = (i==j) ? 1.0 : 0.0;
+        }
+    }
+}
+
+template <typename Float, int Nst, int Nsen >
+void Ukf<Float, Nst, Nsen>::predict( Float * x, FPredict pr )
+{
+    // Obtain Cholesky square root of covariance.
+    cholesky();
+
+    // Initialize weights.
+    const Float lambda = alpha*alpha*(static_cast<Float>(Nst) + k) - static_cast<Float>(Nst);
+    const Float lambda_2 = sqrt( lambda + static_cast<Float>(Nst) );
+    const Float Wm0 = lambda / ( lambda + static_cast<Float>(Nst) );
+    const Float Wc0 = Wm0 + (1.0 - alpha*alpha + beta);
+    const Float Wmci = 0.5/( lambda + static_cast<Float>(Nst) );
+
+    // Generate sigma points.
+    // 0-th point.
+    for ( auto i=0; i<Nst; i++ )
+        sigma[0][i] = x[i];
+    // Predict 0-th sigma point.
+    pr( sigma[0], y[0] );
+    // i-th and i+Nst-th point pairs.
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nst; j++ )
+        {
+            Float d = lambda_2 * L[j][i];
+            sigma[i+1][j]     = x[j] + d;
+            sigma[i+1+Nst][j] = x[j] - d;
+        }
+        // Predict (i+1)-th and (i+1+Nst)-th sigma points.
+        pr( sigma[i+1],     y[i+1] );
+        pr( sigma[i+1+Nst], y[i+1+Nst] );
+    }
+
+    // Calculate mean "Ym".
+    for ( auto i=0; i<Nst; i++ )
+    {
+        const Float W = (i==0) ? Wm0 : Wmci;
+        Ym[i] = 0.0;
+        const int N = 2*Nst+1;
+        for ( auto j=0; j<N; j++ )
+            Ym[i] += W*y[j][i];
+    }
+    // Initialize covatiance Py.
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nst; j++ )
+        {
+            Py[i][j] = Rx[i][j];
+        }
+    }
+    // Calculate covariance "Py" using points "y[i]" and mean value "Ym".
+    const int N = 2*Nst+1;
+    for ( auto k=0; k<N; k++ )
+    {
+        Float W = (k==0) ? Wc0 : Wmci;
+        for ( auto i=0; i<Nst; i++ )
+        {
+            for ( auto j=0; j<Nst; j++ )
+                Py[i][j] += W * ( y[k][i] - Ym[i] ) * ( y[k][j] - Ym[j] );
+        }
+    }
+}
+
+template <typename Float, int Nst, int Nsen >
+void Ukf<Float, Nst, Nsen>::correct( Float * z, FSens    sens )
+{
+    // Predict sensor readings.
+    const int N = 2*Nst+1;
+    for ( auto i=0; i<N; i++ )
+        sens( y[i], zy[i] );
+
+    const Float lambda = alpha*alpha*(static_cast<Float>(Nst) + k) - static_cast<Float>(Nst);
+    const Float lambda_2 = sqrt( lambda + static_cast<Float>(Nst) );
+    const Float Wm0 = lambda / ( lambda + static_cast<Float>(Nst) );
+    const Float Wc0 = Wm0 + (1.0 - alpha*alpha + beta);
+    const Float Wmci = 0.5/( lambda + static_cast<Float>(Nst) );
+
+    // Calculate mean "Zm".
+    for ( auto i=0; i<Nsen; i++ )
+    {
+        Float W = (i==0) ? Wm0 : Wmci;
+        Zm[i] = 0.0;
+        for ( auto j=0; j<N; j++ )
+            Zm[i] += W*zy[j][i];
+    }
+
+    // Calculate sensor noise covariance.
+    // Initialize covatiance Pz.
+    for ( auto i=0; i<Nsen; i++ )
+    {
+        for ( auto j=0; j<Nsen; j++ )
+        {
+            Pz[i][j] = Rz[i][j];
+        }
+    }
+    // Calculate covariance "Pz" using points "z[i]" and mean value "Zm".
+    for ( auto k=0; k<N; k++ )
+    {
+        Float W = (k==0) ? Wc0 : Wmci;
+        for ( auto i=0; i<Nsen; i++ )
+        {
+            for ( auto j=0; j<Nsen; j++ )
+            {
+                Pz[i][j] += W * ( zy[k][i] - Zm[i] ) * ( zy[k][j] - Zm[j] );
+            }
+        }
+    }
+    // Calculate cross covariance Pxz.
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nsen; j++ )
+        {
+            Pyz[i][j] = 0.0;
+        }
+    }
+    for ( auto k=0; k<N; k++ )
+    {
+        Float W = (k==0) ? Wc0 : Wmci;
+        for ( auto i=0; i<Nst; i++ )
+        {
+            for ( auto j=0; j<Nsen; j++ )
+            {
+                Pyz[i][j] += W * ( y[k][i] - Ym[i] ) * ( zy[k][j] - Zm[j] );
+            }
+        }
+    }
+
+    // Calculate inversion invPzz.
+    Math::Matrix<Nsen, Float> A;
+    for ( auto i=0; i<Nsen; i++ )
+    {
+        for ( auto j=0; j<Nsen; j++ )
+        {
+            A[i][j] = Pz[i][j];
+        }
+    }
+    Math::Matrix<Nsen, Float> invA = A.inv();
+    for ( auto i=0; i<Nsen; i++ )
+    {
+        for ( auto j=0; j<Nsen; j++ )
+        {
+            invPz[i][j] = invA[i][j];
+        }
+    }
+
+    // K = Pyz * invPz;
+    Float K[Nst][Nsen];
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nsen; j++ )
+        {
+            K[i][j] = 0.0;
+            for ( auto k=0; k<Nsen; k++ )
+                K[i][j] += Pyz[i][k] * invPz[k][j];
+        }
+    }
+    // Y = Y + K*( z - z(Y) )
+    Float zzy[Nsen];
+    for ( auto i=0; i<Nsen; i++ )
+        zzy[i] = z[i] - Zm[i];
+    for ( auto i=0; i<Nst; i++ )
+    {
+        this->x[i] = Ym[i];
+        for ( auto j=0; j<Nsen; j++ )
+            this->x[i] += K[i][j] * zzy[j];
+    }
+
+    // P = Py - K * Pz * tr(K);
+    Float PzTrK[Nsen][Nst];
+    for ( auto i=0; i<Nsen; i++ )
+    {
+        for ( auto j=0; j<Nst; j++ )
+        {
+            PzTrK[i][j] = 0.0;
+            for ( auto k=0; k<Nsen; k++ )
+                PzTrK[i][j] += Pz[i][k] * Pyz[j][k]; // Second index because it is transposed.
+        }
+    }
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nst; j++ )
+        {
+            P[i][j] = Py[i][j];
+            for ( auto k=0; k<Nsen; k++ )
+            {
+                P[i][j] -= K[i][k] * PzTrK[k][j];
+            }
+        }
+    }
+}
+
+template <typename Float, int Nst, int Nsen >
+void Ukf<Float, Nst, Nsen>::cholesky()
+{
+    // Init elements with zeros.
+    for ( auto i=0; i<Nst; i++ )
+    {
+        for ( auto j=0; j<Nst; j++ )
+        {
+            L[i][j] = 0.0;
+        }
+    }
+
+    // Elements to the left of diagonal.
+    for ( auto i=0; i<Nst; i++ )
+    {
+        Float acc;
+        for ( auto j=0; j<i; j++ )
+        {
+            acc = 0.0;
+            for ( auto k=0; k<j; k++ )
+                acc += L[i][k] * L[j][k];
+            L[i][j] = ( L[i][j] - acc ) / L[j][j];
+        }
+
+        // Diagonal element.
+        acc = P[i][i];
+        for ( auto k=0; k<i; k++ )
+            acc -= L[i][k] * L[i][k];
+        L[i][i] = sqrt( acc );
+    }
+}
+
 
 
 #endif
